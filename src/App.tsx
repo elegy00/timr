35920a984bbb35d56f18ad0d7fd7ffbcaf7b2d7e
christoph <christoph.salvisberg@gmail.com>
2023-10-21 08:35:29 +0200

    Add TW

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  // const onTest = useCallback(async () => {
+  //   navigator.storage &&
+  //     navigator.storage.persist &&
+  //     navigator.storage.persist();
+  //   const root = await navigator.storage.getDirectory();
+  //   const fl = await root.getFileHandle("test.txt", { create: true });
+  //   const flWr = await fl.createWritable({ keepExistingData: true });
+  //   console.log("lFil", flWr);
+  //   await flWr.write("Test");
+  //   flWr.close();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  //   const fl2 = await root.getFileHandle("test.txt");
+  //   const flWr2 = await fl2.getFile();
+  //   const res2 = await flWr2.text();
+  //   console.log("2", res2);
+  // }, []);
+
+  return <div className="bg-red-500">test</div>;
 }
 
-export default App
+export default App;
